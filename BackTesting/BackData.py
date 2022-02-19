c47524437f9ae3fd4a1a92a6255403a7cb235ed9
donghuna <donghuna@naver.com>
2022-02-20 00:00:23 +0900

    save excel

--- BackTesting/BackData.py
+++ BackTesting/BackData.py
@@ -1,5 +1,16 @@
 import Bithumb
+import pandas as pd
+
 
 bithumb = Bithumb.Bithumb()
 df = bithumb.get_ochlv("BTC")
+df['range'] = (df['high'] - df['low']) * 0.5
+
+
+writer = pd.ExcelWriter('Backdata.xlsx', engine='xlsxwriter')
+
 print(df.tail())
+print(type(df.tail()))
+
+df.to_excel(writer, sheet_name='Sheet1')
+writer.close()
